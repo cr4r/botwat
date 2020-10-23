5a5640420fe33872275ea98d46411c33ade64fd4
cr4r1 <ahmadfatchurrachman@mhs.mdp.ac.id>
2020-10-23 13:01:51 +0000

    update

--- msgHndlr.js
+++ msgHndlr.js
@@ -79,7 +79,7 @@ module.exports = msgHandler = async (client, message) => {
         const groupAdmins = isGroupMsg ? await client.getGroupAdmins(groupId) : ''
         const isGroupAdmins = isGroupMsg ? groupAdmins.includes(sender.id) : false
         const isBotGroupAdmins = isGroupMsg ? groupAdmins.includes(botNumber + '@c.us') : false
-        const ownerNumber = '6282282869@c.us'
+        const ownerNumber = '6282282869204@c.us'
         const isOwner = sender.id === ownerNumber
         const isBlocked = blockNumber.includes(sender.id)
         const isNsfw = isGroupMsg ? nsfw_.includes(chat.id) : false
@@ -111,6 +111,7 @@ module.exports = msgHandler = async (client, message) => {
                     else{
                         let messageIndex = body.indexOf(nomor) + nomor.length;
                         let message = body.slice(messageIndex, body.length);
+                        console.log(`Pesan :${message}\nNomor: ${nomor}`)
                         for(i=0;i<limit;i++){
                             client.sendText(nomor+'@c.us',message)
                         }
@@ -128,6 +129,7 @@ module.exports = msgHandler = async (client, message) => {
                 else{
                     let messageIndex = body.indexOf(nomor) + nomor.length;
                     let message = body.slice(messageIndex, body.length);
+                    console.log(`Pesan :${message}\nNomor: ${nomor}`)
                     for(i=0;i<limit;i++){
                         client.sendText(nomor+'@c.us',message)
                     }
