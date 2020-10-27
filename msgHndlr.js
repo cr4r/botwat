a060c3582c622cf7bb654692dd77675e7ab70a11
cr4r1 <ahmadfatchurrachman@mhs.mdp.ac.id>
2020-10-27 05:00:53 +0000

    update

--- msgHndlr.js
+++ msgHndlr.js
@@ -461,11 +461,19 @@ Contoh Penggunaan: ͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏�
                                 console.log(`SEDANG MENGAMBIL GAMBAR di ${imag}`)
                                 os.execCommand(`wget -O media/file/thumb.jpg ${imag}`).then(res=> {
                                     console.log(`SEDANG MENGIRIM GAMBAR DI ./media/file/thumb.jpg`)
-                                    client.sendImage(from, `./media/file/thumb.jpg`, `thumb.jpg`, '➸ *Title* : ${title}\n\nSilahkan tunggu sebentar proses pengiriman file membutuhkan waktu beberapa menit.', id).then(res=> {
-                                        console.log(`SEDANG MENGIRIM MUSIK DI ./media/file/${judul}.mp3`)
-                                        client.sendFile(from, `media/file/${judul}.mp3`, `${namaFile}`, id).catch(err=> {client.reply(`error gan saat mengirim lagu ${judul}.mp3\n\n${err}`)});
-                                        os.execCommand(`rm ./media/file/${judul}.mp3`);
-                                    }).catch(err=> {client.reply(`error gan saat mengirim gambar ${judul}.jpg\n\n${err}`)});
+                                    try{
+                                        client.sendImage(from, `media/file/thumb.jpg`, `thumb.jpg`, `➸ *Judul* : ${judul}\n\nSilahkan tunggu sebentar proses pengiriman file membutuhkan waktu beberapa menit.`, id)
+                                        try{
+                                            console.log(`SEDANG MENGIRIM MUSIK DI ./media/file/${judul}.mp3`)
+                                            client.sendFile(from, `media/file/${judul}.mp3`, `${namaFile}`, id)
+                                            os.execCommand(`rm ./media/file/${judul}.mp3`)
+                                        } catch(err){
+                                            client.reply(`error gan saat mengirim lagu ${judul}.mp3\n\n${err}`)
+                                        }
+                                    }catch (err){
+                                        client.reply(`error gan saat mengirim gambar ${judul}.jpg\n\n${err}`)
+                                    }
+                                        
                                 }).catch(err=> {client.reply(`error gan saat ambil gambar\n\n${err}`)});
                             }).catch(err=> {client.reply(`error gan saat ambil file dari link ${linknya}\n\n${err}`)});
                         }
