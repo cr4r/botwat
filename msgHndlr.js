2f968bf329113b7ea8822df0ef352758c22d4058
cr4r1 <ahmadfatchurrachman@mhs.mdp.ac.id>
2020-10-29 12:16:10 +0000

    update

--- msgHndlr.js
+++ msgHndlr.js
@@ -445,7 +445,7 @@ Contoh Penggunaan: ͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏�
                 '-pointsize',
                 '22',
                 '-interline-spacing',
-                '2',
+                '23',
                 '-annotate',
                 '+170+222',
                 fixHeight,
