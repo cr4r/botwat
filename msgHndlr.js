cf5b2ab6c078c4fa120566de1c46c3b0e80a60df
Ubuntu <ubuntu@cr4r.me>
2020-11-22 09:01:31 +0000

    $1

--- msgHndlr.js
+++ msgHndlr.js
@@ -135,7 +135,7 @@ module.exports = msgHandler = async (client, message) => {
             if(args.length === 1) return client.reply(from,`acl help/..`,id)
             var kdNegara = body.split(' ')[1]
             
-            if(psny.toLowerCase()==='help') return client.sendFile(from,'tools/kodeNegara.txt','kodeNegara.txt',id)
+            if(kdNegara.toLowerCase()==='help') return client.sendFile(from,'tools/kodeNegara.txt','kodeNegara.txt',id)
             formt = `Format:\n1. Apache .htaccess Deny\n2. Apache .htaccess Allow\n3. CIDR\n4. Cisco ACL\n5. Cisco bit bucket\n6. Decimal/CIDR\n7. IP Range\n8. Inverse Netmask\n9. Juniper Junos\n10. Linux iptables\n11. MicroTik\n12. Netmask\n13. Network-Object\n14. Peerguardian2\n15. web.config Deny\n16. web.config allow\n17. Custom CIDR\n18. Custom Netmask`
             
             if(args.length === 2) return client.reply(from,`acl [format] [kode Negara]\nContoh:\nacl 3 CN\n\n${formt}`,id)
