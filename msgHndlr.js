e2ccdbdb072ee2225c42a6713c0458b34e5e78a4
Ubuntu <ubuntu@cr4r.me>
2020-11-22 10:58:48 +0000

    $1

--- msgHndlr.js
+++ msgHndlr.js
@@ -139,7 +139,7 @@ module.exports = msgHandler = async (client, message) => {
             formt = `Format:\n1. Apache .htaccess Deny\n2. Apache .htaccess Allow\n3. CIDR\n4. Cisco ACL\n5. Cisco bit bucket\n6. Decimal/CIDR\n7. IP Range\n8. Inverse Netmask\n9. Juniper Junos\n10. Linux iptables\n11. MicroTik\n12. Netmask\n13. Network-Object\n14. Peerguardian2\n15. web.config Deny\n16. web.config allow\n17. Custom CIDR\n18. Custom Netmask`
             
             if(args.length === 2) return client.reply(from,`acl [format] [kode Negara]\nContoh:\nacl CN 3\n\n${formt}`,id)
-            var formtny = body.split(' ')[2]
+            var formtny = body.split(' ')[2].trim()
 
             if(formtny > 18) return client.reply(from, formt,id)
             var kode = fs.readFileSync('tools/kodeNegara.txt','utf-8').trim().split('\n')
--- tools/aclIp.js
+++ tools/aclIp.js
@@ -3,26 +3,47 @@ var request = require('request')
 var cheerio = require('cheerio');
 var fs = require('fs-extra')
 
-module.exports = doing = (linkn,format) => new Promise((resolve, reject) => {
-    if(format === 1){ formatt = 3 }
-    else if(format === 2)  { formatt = 4 }
-    else if(format === 3)  { formatt = 1 }
-    else if(format === 4)  { formatt = 8 }
-    else if(format === 5)  { formatt = 14 }
-    else if(format === 6)  { formatt = 6 }
-    else if(format === 7)  { formatt = 5 }
-    else if(format === 8)  { formatt = 16 }
-    else if(format === 9)  { formatt = 15 }
-    else if(format === 10) { formatt = 18 }
-    else if(format === 11) { formatt = 17 }
-    else if(format === 12) { formatt = 2 }
-    else if(format === 13) { formatt = 13 }
-    else if(format === 14) { formatt = 9 }
-    else if(format === 15) { formatt = 11 }
-    else if(format === 16) { formatt = 12 }
-    else if(format === 17) { formatt = 19 }
-    else if(format === 18) { formatt = 20 }
-    else { resolve({'status':'no','result':format}) }
+module.exports = doing = (linkn,formatn) => new Promise((resolve, reject) => {
+    var formatt = ''
+    if(formatn === 1) {
+        formatt = 3
+    }else if(formatn === 2){
+        formatt = 4
+    }else if(formatn === 3){
+        formatt = 1
+    }else if(formatn === 4){
+        formatt = 8
+    }else if(formatn === 5)  {
+        formatt = 14
+    }else if(formatn === 6){
+        formatt = 6
+    }else if(formatn === 7){
+        formatt = 5
+    }else if(formatn === 8){
+        formatt = 16
+    }else if(formatn === 9)  {
+        formatt = 15
+    }else if(formatn === 10) {
+        formatt = 18
+    }else if(formatn === 11) {
+        formatt = 17
+    }else if(formatn === 12) {
+        formatt = 2
+    }else if(formatn === 13) {
+        formatt = 13
+    }else if(formatn === 14) {
+        formatt = 9
+    }else if(formatn === 15) {
+        formatt = 11
+    }else if(formatn === 16) {
+        formatt = 12
+    }else if(formatn === 17) {
+        formatt = 19
+    }else if(formatn === 18) {
+        formatt = 20
+    }else {
+        resolve({'status':'no','result':formatn})
+    }
 
     var headers = {
         'User-Agent':       'Super Agent/0.0.1',
