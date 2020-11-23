4398cb90f193c66176cbc604169118c70966041c
Ubuntu <ubuntu@cr4r.me>
2020-11-23 02:05:16 +0000

    $1

--- msgHndlr.js
+++ msgHndlr.js
@@ -451,6 +451,7 @@ module.exports = msgHandler = async (client, message) => {
             if(pl==='des'){
                 var pili = body.split('des ')[1].split(' ')[0]
                 var pesan = body.split('des ')[1].slice(pili.length+1)
+                console.log(pesan)
                 if(pili==='asci'){
                     let psn = ""
                     for (var i = 0, len = pesan.length; i < len; i++) {
@@ -465,6 +466,7 @@ module.exports = msgHandler = async (client, message) => {
             else if(pl === 'bin'){
                 var pili = body.split('bin ')[1].split(' ')[0]
                 var pesan = body.split('bin ')[1].slice(pili.length+1)
+                console.log(pesan)
                 if(pili === 'hex'){
                     var ps = parseInt(pesan,2).toString(16)
                     if(ps === 'NaN') return client.reply(from, 'Seharusnya anda memasukkan angka binary bukan huruf',id)
@@ -477,6 +479,7 @@ module.exports = msgHandler = async (client, message) => {
             }else if(pl === 'hex'){
                 var pili = body.split('hex ')[1].split(' ')[0]
                 var pesan = body.split('hex ')[1].slice(pili.length+1)
+                console.log(pesan)
                 if(pili === 'des'){
                     ps = parseInt(pesan, 8)
                     if(ps === 'NaN') return client.reply(from, 'Seharusnya anda memasukkan angka hex bukan huruf',id)
